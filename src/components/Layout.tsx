a691dab578361c03fca723c480760cc26eb80776
pdesai07 <128464821+pdesai07@users.noreply.github.com>
2025-08-05 13:21:31 +0530

    updated code for deployment to webpage

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,19 @@
 import { ReactNode } from 'react'
+import Head from 'next/head'
 import Sidebar from './Sidebar'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex min-h-screen bg-[#efeee7] dark:bg-[#0e0e10] text-black dark:text-white">
-      <Sidebar />
-      <main className="ml-72 w-full p-6">{children}</main>
-    </div>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Priyansh Desai</title>
+      </Head>
+
+      <div className="flex min-h-screen bg-[#efeee7] dark:bg-[#0e0e10] text-black dark:text-white">
+        <Sidebar />
+        <main className="ml-72 w-full p-6">{children}</main>
+      </div>
+    </>
   )
 }
